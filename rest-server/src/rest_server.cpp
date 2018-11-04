eb7675980276fb97eb89184362745206f02fbb14
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-11-04 12:23:46 -0600

    Example showing the use of two controllers
    - Removed many unneeded includes and using statements in network_utils

--- rest-server/src/rest_server.cpp
+++ rest-server/src/rest_server.cpp
@@ -3,12 +3,15 @@
 #include <iostream>
 #include <string>
 #include "example_controller.hpp"
+#include "another_controller.hpp"
 using aoi_rest::ExampleController;
+using aoi_rest::AnotherController;
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Entering into main method." << std::endl;
-    ExampleController server;
-    utility::string_t server_address;
+    std::cout << "Starting aoi rest server" << std::endl;
+    ExampleController example_controller;
+    AnotherController another_controller;
+    std::string server_address;
     if (argc > 2)
         server_address.append(argv[2]);
     else
@@ -18,15 +21,20 @@ int main(int argc, const char * argv[]) {
     else
         server_address.append(":8080");
     server_address.append("/v1/aoi/api");
-    server.endpoint(server_address);
+    example_controller.endpoint(server_address + "/example");
+    another_controller.endpoint(server_address + "/another");
     try {
-        server.Accept().wait();
+        example_controller.Accept().wait();
+        another_controller.Accept().wait();
         std::cout << "Listening for requests at: "
-                << server.endpoint() << std::endl;
+                << example_controller.endpoint() << std::endl
+                << "Listening for requests at: "
+                << another_controller.endpoint() << std::endl;
         std::cout << "Press ENTER to exit." << std::endl;
         std::string line;
         std::getline(std::cin, line);
-        server.Shutdown().wait();
+        example_controller.Shutdown().wait();
+        another_controller.Shutdown().wait();
     }
     catch(std::exception&  e) {
         std::cerr << "There was an error" << std::endl;
