cfdc3ddffc9748f2e1ef2d1f408a5cbf42681a43
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-11-08 12:03:31 -0600

    Users endpoint, GET can now query for specific users
    - By id as '/users/{id}'
    - By name partial string match as 'users/{name}'

--- rest-server/src/controller/user_controller.cpp
+++ rest-server/src/controller/user_controller.cpp
@@ -2,6 +2,7 @@
 //                  Shawn Hulce, Michael McCulley
 #include <cpprest/uri_builder.h>
 #include <cpprest/base_uri.h>
+#include <algorithm>
 #include <iostream>
 #include "user_controller.hpp"
 
@@ -18,14 +19,54 @@ void UserController::HandleGet(http_request message) {
         auto response = json::value::object();
         auto users = DataAccessObject::Instance().GetUsers();
         response["users"] = json::value::object();
-        for (auto& user : users) {
-            auto user_json  = json::value::object();
-            user_json["name"] = json::value::string(user.name);
-            user_json["password"] = json::value::string(user.password);
-            response["users"][std::to_string(user.id)] = user_json;
+        auto queries = Queries(message.relative_uri().query());
+        auto relative_path = message.relative_uri().to_string();
+        // for loop for testing purposes only
+        for (auto const& query : queries)
+            std::cout << "name: " << query.first << " value: " << query.second << std::endl;
+        // end test section
+        if(relative_path.length() > 1 && relative_path.at(1) != '?') {
+            auto next_forward_slash = relative_path.find("/", 1);
+            std::string id_as_string;
+            if(next_forward_slash == std::string::npos)  // no more slashes
+                id_as_string = relative_path.substr(1);
+            else
+                id_as_string = relative_path.substr(1, next_forward_slash - 1);
+            if(id_as_string.find_first_not_of("0123456789") == std::string::npos) {
+                int id = std::stoi(id_as_string);
+                for (auto& user : users) {
+                    if(user.id == id) {
+                        auto user_json  = json::value::object();
+                        user_json["name"] = json::value::string(user.name);
+                        user_json["password"] = json::value::string(user.password);
+                        response["users"][std::to_string(user.id)] = user_json;
+                    }
+                }
+            }
+        } else if(queries.count("name") > 0) {
+            for (auto& user : users) {
+                // this long if statement is needed for case insensitive matches
+                if(std::search(user.name.begin(), user.name.end(),
+                            queries["name"].begin(), queries["name"].end(),
+                            [](char ch1, char ch2) { return std::toupper(ch1) == std::toupper(ch2);
+                            }) != user.name.end()) {
+                    auto user_json  = json::value::object();
+                    user_json["name"] = json::value::string(user.name);
+                    user_json["password"] = json::value::string(user.password);
+                    response["users"][std::to_string(user.id)] = user_json;
+                }
+            }
+        } else {
+            for (auto& user : users) {
+                auto user_json  = json::value::object();
+                user_json["name"] = json::value::string(user.name);
+                user_json["password"] = json::value::string(user.password);
+                response["users"][std::to_string(user.id)] = user_json;
+            }
         }
         message.reply(status_codes::OK, response);
-    } catch (std::exception&  e) {  // for testing purposes
+    } catch (std::exception&  e) {
+        // return error for testing purposes only
         message.reply(status_codes::InternalError, json::value::string(e.what()));
     }
 }
