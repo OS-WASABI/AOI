282c558a7f9b1fe71d822534d6b64229bde867b6
vsagrawal0 <vaniya0@yahoo.com>
2019-02-04 12:21:02 -0600

    Setup initial redux store and userReducer.

---
+++ aoi-ui/src/actions/types.js
@@ -0,0 +1,3 @@
+export const CREATE_USER = 'CREATE-USER';
+export const GET_USER = 'CREATE-USER';
+export const UPDATE_USER = 'CREATE-USER';
---
+++ aoi-ui/src/actions/userActions.js
@@ -0,0 +1,6 @@
+import { CREATE_USER, GET_USER, UPDATE_USER } from "./types";
+
+export function getUser() {
+  return function(dispatch) {
+  }
+}
