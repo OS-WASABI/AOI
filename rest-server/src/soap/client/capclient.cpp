d4c31dced416e68de901f7f9846f785ed51db5ca
Shawn Hulce <shawn_hulce@hotmail.com>
2019-02-06 21:47:37 -0600

    Attempting to create HTTP call to server from gsoap client

--- rest-server/src/soap/client/capclient.cpp
+++ rest-server/src/soap/client/capclient.cpp
@@ -1,5 +1,6 @@
 #include "soapCAPSoapHttpProxy.h"
 #include "CAPSoapHttp.nsmap"
+#include <stdio.h>
 
 const char server[] = "http://localhost:8080/aoi/soap/alerts";
 
@@ -13,11 +14,25 @@ int main(int argc, char **argv)
         exit(0);
     }
     std::string filename = argv[3];
+    struct soap
+    _ns2__alert outboundAlert;
+    struct soap ctx = *soap_new2(SOAP_XML_STRICT, SOAP_XML_INDENT);
+    std::ifstream fileIn("./alert1", std::ifstream::in);
+
+    //ctx.is = &fileIn;
+
+    //soap_read__ns2__alert(&ctx, &outboundAlert);
+
+    //std::cout << "Sender Read: " << outboundAlert.sender << std::endl;
+
     CAPSoapHttpProxy cap;
     cap.soap_endpoint = server;
-    _ns1__postCAPRequestTypeDef ns1__postCAPRequestTypeDef;   // request needs to contain data
-    _ns1__postCAPResponseTypeDef ns1__postCAPResponseTypeDef; // data holder for response
-    cap.postCAP(&ns1__postCAPRequestTypeDef, ns1__postCAPResponseTypeDef);
+    _ns2__postCAPRequestTypeDef postRequest;   // request needs to contain data
+    _ns2__postCAPResponseTypeDef response; // data holder for response
+    
+    cap.send_postCAP(server, *SOAP_POST, postRequest);
+
+    //cap.postCAP(&ns1__postCAPRequestTypeDef, ns1__postCAPResponseTypeDef);
     if (cap.soap->error)
          cap.soap_stream_fault(std::cerr);
      else
