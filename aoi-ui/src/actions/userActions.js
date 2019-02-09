3bf0978f7c8fa769c7d201c87ebce5cad7ab71b6
vsagrawal0 <vaniya0@yahoo.com>
2019-02-08 21:16:58 -0600

    Finalized documentation.

--- aoi-ui/src/actions/userActions.js
+++ aoi-ui/src/actions/userActions.js
@@ -1,10 +1,31 @@
+/// Defines actions the involve user data.
+/**
+ * Defines the actions that can occur to manage
+ * or manipulate user data.
+ * Copyright 2018   Vaniya Agrawal, Ross Arcemont, Kristofer Hoadley,
+ *                  Shawn Hulce, Michael McCulley
+ * @file    userActions.js
+ * @date    February 2019
+ * @authors Vaniya Agrawal
+ */
 import {
   GET_USERS,
   CREATE_USER,
   UPDATE_USER,
   REMOVE_USER } from "./types";
 
+/// Get user data.
+/**
+ * Gets user data from the server. If all user data is being requested,
+ * the query must be null. Otherwise, the query must include the key
+ * and value to search on. For instance:
+ *    { userID: 0 },
+ *    { name: 'Vaniya' }
+ * @param query The search parameter in the form {key: value}
+ * @returns {Function}  Dispatches an action.
+ */
 export const getUser = (query) => dispatch => {
+    // TODO (Vaniya): Build message body or query string based on query.
     fetch('https://jsonplaceholder.typicode.com/users/' + query)
       .then(res => res.json())
       .then(user => dispatch({
@@ -13,6 +34,14 @@ export const getUser = (query) => dispatch => {
       }));
 }
 
+/// Post user data.
+/**
+ * Posts user data to the server. The user data is contained in the body
+ * of the request and contains all necessary information.
+ * @param user  Contains a first_name, last_name, username, email. Optionally,
+ *              has a phone, address, country, state_region, and zip.
+ * @returns {Function}  Dispatches an action.
+ */
 export const createUser = (user) => dispatch => {
   fetch('https://jsonplaceholder.typicode.com/users', {
     method: 'POST',
@@ -28,8 +57,15 @@ export const createUser = (user) => dispatch => {
     }));
 }
 
-export const updateUser = (userData) => dispatch => {
-  fetch('https://jsonplaceholder.typicode.com/users', {
+/// Put user data.
+/**
+ * Puts user data to the server.
+ * @param userData  Contains the data to be modified in the form {key: value, key2: value2, ...}
+ * @param userID    The ID of the user to be modified.
+ * @returns {Function}  Dispatches an action.
+ */
+export const updateUser = (userData, userID) => dispatch => {
+  fetch('https://jsonplaceholder.typicode.com/users/' + userID, {
     method: 'PUT',
     headers: {
       'content-type': 'application/json'
@@ -43,6 +79,12 @@ export const updateUser = (userData) => dispatch => {
     }))
 };
 
+///Remove user data.
+/**
+ * Removes a data from the server.
+ * @param userID  The id of the user to remove.
+ * @returns {Function}  Dispatches an action.
+ */
 export const removeUser = (userID) => dispatch => {
   fetch('https://jsonplaceholder.typicode.com/users', {
     method: 'POST',
--- aoi-ui/src/reducers/index.js
+++ aoi-ui/src/reducers/index.js
@@ -1,3 +1,16 @@
+/// The root reducer.
+/**
+ * The root reducer for the redux store. Combines all other reducers
+ * from this directory into one root reducer.
+ *
+ * Copyright 2018   Vaniya Agrawal, Ross Arcemont, Kristofer Hoadley
+ *                  Shawn Hulce, Michael McCulley
+ *
+ * @file    index.js
+ * @date    February 2019
+ * @authors Vaniya Agrawal
+ */
+
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import alertReducer from './alertReducer';
