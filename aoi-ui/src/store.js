3784d6160fad5a6daa12ce724ba4a9213f2f6e36
vsagrawal0 <vaniya0@yahoo.com>
2019-02-08 21:45:23 -0600

    Integrate react-router-dom and connected-react-router for creating routes.

--- aoi-ui/src/store.js
+++ aoi-ui/src/store.js
@@ -13,15 +13,21 @@
  * @authors Vaniya Agrawal
  */
 import { createStore, applyMiddleware, compose } from 'redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory';
 import rootReducer from './reducers';
 
+export const history = createHistory();
 
 const initialState = {};  // Represents the root of the store
-const middleware = [thunk]; // Specifies which middleware it being utilized
+const middleware = [
+  thunk,
+  routerMiddleware(history)
+]; // Specifies which middleware is being utilized
 
 const store = createStore(
-  rootReducer,
+  connectRouter(history)(rootReducer),
   initialState,
   compose(
     applyMiddleware(...middleware),
