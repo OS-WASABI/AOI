fd40c3e02a8e397a011e57feaf2eb8d26e94776b
Ross <rosarc@gmail.com>
2019-02-09 19:56:13 -0600

    Finished adding optional node visibility conditions
    - Added code documentation

--- soap/cap/cap.cpp
+++ soap/cap/cap.cpp
@@ -1,10 +1,24 @@
-//
-// Created by Ross on 1/31/2019.
-//
+// Copyright 2019   Vaniya Agrawal, Ross Arcemont, Kristofer Hoadley,
+//                  Shawn Hulce, Michael McCulley
+/**
+ * @file        cap.cpp
+ * @authors     Ross Arcemont
+ * @date        February, 2019
+ */
 
 #include "cap.hpp"
 
+///Creates a CAP message from a provided CAP object.
+/**
+ * Takes in a CAP object containing all necessary data to create a
+ * CAP XML document. This function assumes that all necessary validation
+ * of the CAP object data has occurred already, as this function does not
+ * provide any such validation.
+ * @param data_input        CAP object providing all necessary data
+ * @param cap_filename      Filepath and filename of the CAP document to be saved
+ */
 void CAP::create_cap_doc(aoi_rest::Alert data_input, std::string cap_filename) {
+    //Creating SOAP envelope for the CAP message
     pugi::xml_document cap_doc;
     auto declaration_node = cap_doc.append_child(pugi::node_declaration);
     declaration_node.append_attribute("version") = "1.0";
@@ -15,6 +29,7 @@ void CAP::create_cap_doc(aoi_rest::Alert data_input, std::string cap_filename) {
     root_node.append_attribute("SOAP-ENV:encodingStyle") = "\"http://www.w3.org/2001/12/soap-encoding\"";
 
     auto soap_body = root_node.append_child("SOAP-ENV:Body");
+    //Creating CAP message main structure with necessary data
     auto alert = soap_body.append_child("alert");
     alert.append_attribute("xmlns") = "\"urn:oasis:names:tc:emergency:cap:1.2\"";
 
@@ -71,10 +86,15 @@ void CAP::create_cap_doc(aoi_rest::Alert data_input, std::string cap_filename) {
         incidents.text().set(data_input.incidents);
     }
 
+    //Creating info blocks with necessary data
     for (int i = 0; i < data_input.info.size(); i++) {
         auto info = alert.append_child("info");
         auto language = info.append_child("language");
-        language.text().set(data_input.info.at(i).language);
+        if (data_input.info.at(i).language.compare("") == 0) {
+            language.text().set("en-US");
+        } else {
+            language.text().set(data_input.info.at(i).language);
+        }
 
         for (int j = 0; j < data_input.info.at(i).categories.size(); j++) {
             auto category = info.append_child("category");
@@ -98,8 +118,10 @@ void CAP::create_cap_doc(aoi_rest::Alert data_input, std::string cap_filename) {
         auto certainty = info.append_child("certainty");
         certainty.text().set(data_input.info.at(i).certainty);
 
-        auto audience = info.append_child("audience");
-        audience.text().set(data_input.info.at(i).audience);
+        if (data_input.info.at(i).audience.compare("") != 0) {
+            auto audience = info.append_child("audience");
+            audience.text().set(data_input.info.at(i).audience);
+        }
 
         for (int j = 0; j < data_input.info.at(i).event_codes.size(); j++) {
             auto event_code = info.append_child("eventCode");
@@ -107,37 +129,60 @@ void CAP::create_cap_doc(aoi_rest::Alert data_input, std::string cap_filename) {
         }
 
         auto effective = info.append_child("effective");
-        effective.text().set(data_input.info.at(i).effective_time);
+        if (data_input.info.at(i).effective.compare("") == 0) {
+            effective.text().set(data_input.sent_time);
+        } else {
+            effective.text().set(data_input.info.at(i).effective_time);
+        }
 
-        auto onset = info.append_child("onset");
-        onset.text().set(data_input.info.at(i).onset_time);
+        if (data_input.info.at(i).onset.compare("") != 0) {
+            auto onset = info.append_child("onset");
+            onset.text().set(data_input.info.at(i).onset_time);
+        }
 
         auto expires = info.append_child("expires");
-        expires.text().set(data_input.info.at(i).expire_time);
+        if (data_input.info.at(i).expires.compare("") == 0) {
+            //TODO: Determine default behavior
+        } else {
+            expires.text().set(data_input.info.at(i).expire_time);
+        }
 
-        auto sender_name = info.append_child("senderName");
-        sender_name.text().set(data_input.info.at(i).sender_name);
+        if (data_input.info.at(i).sender_name.compare("") != 0) {
+            auto sender_name = info.append_child("senderName");
+            sender_name.text().set(data_input.info.at(i).sender_name);
+        }
 
-        auto headline = info.append_child("headline");
-        headline.text().set(data_input.info.at(i).headline);
+        if (data_input.info.at(i).headline.compare("") != 0) {
+            auto headline = info.append_child("headline");
+            headline.text().set(data_input.info.at(i).headline);
+        }
 
-        auto description = info.append_child("description");
-        description.text().set(data_input.info.at(i).description);
+        if (data_input.info.at(i).description.compare("") != 0) {
+            auto description = info.append_child("description");
+            description.text().set(data_input.info.at(i).description);
+        }
 
-        auto instruction = info.append_child("instruction");
-        instruction.text().set(data_input.info.at(i).instruction);
+        if (data_input.info.at(i).instruction.compare("") != 0) {
+            auto instruction = info.append_child("instruction");
+            instruction.text().set(data_input.info.at(i).instruction);
+        }
 
-        auto web = info.append_child("web");
-        web.text().set(data_input.info.at(i).web_url);
+        if (data_input.info.at(i).web_url.compare("") != 0) {
+            auto web = info.append_child("web");
+            web.text().set(data_input.info.at(i).web_url);
+        }
 
-        auto contact = info.append_child("contact");
-        contact.text().set(data_input.info.at(i).contact);
+        if (data_input.info.at(i).contact.compare("") != 0) {
+            auto contact = info.append_child("contact");
+            contact.text().set(data_input.info.at(i).contact);
+        }
 
         for (int j = 0; j < data_input.info.at(i).parameters.size(); j++) {
             auto parameter = info.append_child("parameter");
             parameter.text().set(data_input.info.at(i).parameters.at(j));
         }
 
+        //Creating resource blocks with necessary data
         for (int j = 0; j < data_input.info.at(i).resources.size(); j++) {
             auto resource = info.append_child("resource");
             auto resource_desc = resource.append_child("resourceDesc");
@@ -146,19 +191,28 @@ void CAP::create_cap_doc(aoi_rest::Alert data_input, std::string cap_filename) {
             auto mime_type = resource.append_child("mimeType");
             mime_type.text().set(data_input.info.at(i).resources.at(j).mime_type);
 
-            auto size = resource.append_child("size");
-            size.text().set(data_input.info.at(i).resources.at(j).size);
+            if (data_input.info.at(i).resources.at(j).size > 0) {
+                auto size = resource.append_child("size");
+                size.text().set(data_input.info.at(i).resources.at(j).size);
+            }
 
-            auto uri = resource.append_child("uri");
-            uri.text().set(data_input.info.at(i).resources.at(j).uri);
+            if (data_input.info.at(i).resources.at(j).uri.compare("") != 0) {
+                auto uri = resource.append_child("uri");
+                uri.text().set(data_input.info.at(i).resources.at(j).uri);
+            }
 
-            auto dereferenced_uri = resource.append_child("derefUri");
-            dereferenced_uri.text().set(data_input.info.at(i).resources.at(j).dereferenced_uri);
+            if (data_input.info.at(i).resources.at(j).dereferenced_uri.compare("") != 0) {
+                auto dereferenced_uri = resource.append_child("derefUri");
+                dereferenced_uri.text().set(data_input.info.at(i).resources.at(j).dereferenced_uri);
+            }
 
-            auto digest = resource.append_child("digest");
-            digest.text().set(data_input.info.at(i).resources.at(j).digest);
+            if (data_input.info.at(i).resources.at(j).digest.compare("") != 0) {
+                auto digest = resource.append_child("digest");
+                digest.text().set(data_input.info.at(i).resources.at(j).digest);
+            }
         }
 
+        //Creating area blocks with necessary data
         for (int j = 0; j < data_input.info.at(i).areas.size(); j++) {
             auto area = info.append_child("area");
             auto area_desc = area.append_child("areaDesc");
@@ -179,11 +233,16 @@ void CAP::create_cap_doc(aoi_rest::Alert data_input, std::string cap_filename) {
                 geocode.text().set(data_input.info.at(i).areas.at(j).geocode.at(k));
             }
 
-            auto altitude = area.append_child("altitude");
-            altitude.text().set(data_input.info.at(i).areas.at(j).altitude);
+            //TODO: Determine if parsing the lower limit from <ceiling> is necessary.
+            if (data_input.info.at(i).areas.at(j).altitude.compare("") != 0) {
+                auto altitude = area.append_child("altitude");
+                altitude.text().set(data_input.info.at(i).areas.at(j).altitude);
+            }
 
-            auto ceiling = area.append_child("ceiling");
-            ceiling.text().set(data_input.info.at(i).areas.at(j).ceiling);
+            if (data_input.info.at(i).areas.at(j).altitude.compare("") != 0 && data_input.info.at(i).areas.at(j).ceiling.compare("") != 0) {
+                auto ceiling = area.append_child("ceiling");
+                ceiling.text().set(data_input.info.at(i).areas.at(j).ceiling);
+            }
         }
     }
 }
--- soap/cap/cap.hpp
+++ soap/cap/cap.hpp
@@ -1,6 +1,10 @@
-//
-// Created by Ross on 1/31/2019.
-//
+// Copyright 2019   Vaniya Agrawal, Ross Arcemont, Kristofer Hoadley,
+//                  Shawn Hulce, Michael McCulley
+/**
+ * @file        cap.hpp
+ * @authors     Ross Arcemont
+ * @date        February, 2019
+ */
 
 #include <iostream>
 #include <string>
@@ -12,9 +16,22 @@
 #ifndef AOI_REST_SERVER_CAP_H
 #define AOI_REST_SERVER_CAP_H
 
-
+///Provides mechanisms through a static create_cap_doc method to create a CAP XML document using a provided CAP object
+/**
+ * Creates a syntactically correct SOAP CAP document using a pre-validated CAP object.
+ * Example use: CAP::create_cap_doc(cap_object, "../file/path/to/save/capname.xml");
+ */
 class CAP {
 public:
+    ///Creates a CAP message from a provided CAP object.
+    /**
+     * Takes in a CAP object containing all necessary data to create a
+     * CAP XML document. This function assumes that all necessary validation
+     * of the CAP object data has occurred already, as this function does not
+     * provide any such validation.
+     * @param data_input        CAP object providing all necessary data
+     * @param cap_filename      Filepath and filename of the CAP document to be saved
+     */
     static void create_cap_doc(aoi_rest::Alert data_input, std::string cap_filename);
 };
 
