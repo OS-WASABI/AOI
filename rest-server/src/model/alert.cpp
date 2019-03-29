2334bd36d794919f3f54c27f95ba5b8af4be2d34
asumike <mbmccull@asu.edu>
2019-03-28 21:58:35 -0700

    More refactoring. This is embarassing at this point

--- rest-server/src/model/alert.cpp
+++ rest-server/src/model/alert.cpp
@@ -20,10 +20,12 @@
 
 namespace aoi_rest {
 
-std::optional<std::vector<std::string>> json_array_to_string_vector_optional(web::json::array json_array) {
+std::optional<std::vector<std::string>> json_value_to_string_vector_optional(web::json::value json_value) {
+    if (!json_value.is_array())
+        return std::nullopt;
     std::optional<std::vector<std::string>> temp_vector;
-    for (auto json_element = json_array.begin();
-         json_element != json_array.end(); ++json_element) {
+    for (auto json_element = json_value.as_array().begin();
+         json_element != json_value.as_array().end(); ++json_element) {
         if (json_element->is_string()) {
             temp_vector.value().push_back(json_element->as_string());
         } else {
@@ -38,144 +40,106 @@ std::optional<Alert> Alert::from_json(web::json::value alert_json) {
         Alert alert = Alert();
 
         // Required
-        if (alert_json.has_field("identifier") && alert_json["identifier"].is_string()) {
-            if (!alert.validate_identifier(alert_json["identifier"].as_string()))
-                return std::nullopt;
-        } else {
+        if (!alert_json.has_field("identifier") && !alert.validate_identifier(""))
             return std::nullopt;
-        }
+        if (!alert_json["identifier"].is_string())
+            return std::nullopt;
+        if (!alert.validate_identifier(alert_json["identifier"].as_string()))
+            return std::nullopt;
 
         // Required
-        if (alert_json.has_field("sender") && alert_json["sender"].is_string()) {
-            if (!alert.validate_sender(alert_json["sender"].as_string()))
-                return std::nullopt;
-        } else {
+        if (!alert_json.has_field("sender") && !alert.validate_sender(""))
             return std::nullopt;
-        }
+        if (!alert_json["sender"].is_string())
+            return std::nullopt;
+        if (!alert.validate_sender(alert_json["sender"].as_string()))
+            return std::nullopt;
 
         // Required
-        if (alert_json.has_field("sent_time") && alert_json["sent_time"].is_string()) {
-            struct tm tm;
-            strptime(alert_json["sent_time"].as_string().c_str(), "%FT%T.000Z", &tm);
-            time_t sent_time = mktime(&tm);
-        } else {
+        if (!alert_json.has_field("sent_time") && !alert.validate_sent_time(0))
             return std::nullopt;
-        }
+        if (!alert_json["sent_time"].is_string())
+            return std::nullopt;
+        struct tm tm;
+        strptime(alert_json["sent_time"].as_string().c_str(), "%FT%T.000Z", &tm);
+        time_t sent_time = mktime(&tm);
+        if (!alert.validate_sent_time(sent_time))
+            return std::nullopt;
 
         //Required
-        if (alert_json.has_field("status") && alert_json["status"].is_string()) {
-            if (!alert.validate_status(alert_json["status"].as_string()))
-                return std::nullopt;
-        } else {
+        if (!alert_json.has_field("status") && !alert.validate_status(""))
             return std::nullopt;
-        }
+        if (!alert_json["status"].is_string())
+            return std::nullopt;
+        if (!alert.validate_status(alert_json["status"].as_string()))
+            return std::nullopt;
 
         // Required
-        if (alert_json.has_field("msg_type") && alert_json["msg_type"].is_string()) {
-            if (!alert.validate_msg_type(alert_json["msg_type"].as_string()))
-                return std::nullopt;
-        } else {
+        if (alert_json.has_field("msg_type") && alert.validate_msg_type(""))
             return std::nullopt;
-        }
+        if (!alert_json["msg_type"].is_string())
+            return std::nullopt;
+        if (!alert.validate_msg_type(alert_json["msg_type"].as_string()))
+            return std::nullopt;
+
 
         // Required
-        if (alert_json.has_field("scope") && alert_json["scope"].is_string()) {
-            if (!alert.validate_scope(alert_json["scope"].as_string()))
-                return std::nullopt;
-        } else {
+        if (!alert_json.has_field("scope") && !alert.validate_scope(""))
             return std::nullopt;
-        }
+        if (!alert_json["scope"].is_string())
+            return std::nullopt;
+        if (!alert.validate_scope(alert_json["scope"].as_string()))
+            return std::nullopt;
 
         // Required per CAP IPAWS Profile
-        if (alert_json.has_field("handling_codes") && alert_json["handling_codes"].is_array()) {
-            if (std::optional<std::vector<std::string>> temp_vector = json_array_to_string_vector_optional(
-                    alert_json["handling_codes"].as_array())) {
-                if (!alert.validate_handling_codes(temp_vector.value()))
-                    return std::nullopt;
-            } else {
-                return std::nullopt;
-            }
-        } else {
+        std::optional<std::vector<std::string>> codes_vector = json_value_to_string_vector_optional(alert_json["handling_codes"]);
+        if (!codes_vector)
             return std::nullopt;
-        }
+        if (!alert.validate_handling_codes(codes_vector.value()))
+            return std::nullopt;
 
         // Conditional
-        if (alert_json.has_field("restriction")) {
-            if (alert_json["restriction"].is_string()) {
-                if (!alert.validate_restriction(alert_json["restrction"].as_string()))
-                    return std::nullopt;
-            } else {
-                return std::nullopt;
-            }
-        } else {
-            // Required when scope is restricted
-            if (alert.scope__.value() == "Restricted")
-                return std::nullopt;
-        }
+        if (!alert_json.has_field("restriction") && !alert.validate_restriction(""))
+            return std::nullopt;
+        if (!alert_json["restriction"].is_string())
+            return std::nullopt;
+        if (!alert.validate_restriction(alert_json["restrction"].as_string()))
+            return std::nullopt;
 
         // Conditional
-        if (alert_json.has_field("addresses")) {
-            if (alert_json["addresses"].is_array()) {
-                if (std::optional<std::vector<std::string>> temp_vector = json_array_to_string_vector_optional(
-                        alert_json["addresses"].as_array())) {
-                    if (!alert.validate_addresses(temp_vector.value()))
-                        return std::nullopt;
-                } else {
-                    return std::nullopt;
-                }
-            } else {
-                return std::nullopt;
-            }
-        } else {
-            // Required when scope is private
-            if (alert.scope__.value() == "Public")
-                return std::nullopt;
-        }
+        std::optional<std::vector<std::string>> addresses_vector = json_value_to_string_vector_optional(alert_json["addresses"]);
+        if (!addresses_vector)
+            return std::nullopt;
+        if (!alert.validate_addresses(addresses_vector.value()))
+            return std::nullopt;
 
         // Optional
-        if (alert_json.has_field("source")) {
-            if (alert_json["source"].is_string()) {
-                alert.validate_source(alert_json["source"].as_string());
-            } else {
-                return std::nullopt;
-            }
-        }
+        if (!alert_json.has_field("source") && !alert.validate_source(""))
+            return std::nullopt;
+        if (!alert_json["source"].is_string())
+            return std::nullopt;
+        if (!alert.validate_source(alert_json["source"].as_string()))
+            return std::nullopt;
 
-        if (alert_json.has_field("note")) {
-            if (alert_json["note"].is_string()) {
-                alert.validate_note(alert_json["note"].as_string());
-            } else {
-                return std::nullopt;
-            }
-        }
 
-        if (alert_json.has_field("references")) {
-            if (alert_json["references"].is_array()) {
-                if (std::optional<std::vector<std::string>> temp_vector = json_array_to_string_vector_optional(
-                        alert_json["references"].as_array())) {
-                    if (!alert.validate_references(temp_vector.value()))
-                        return std::nullopt;
-                } else {
-                    return std::nullopt;
-                }
-            } else {
-                return std::nullopt;
-            }
-        }
+        if (!alert_json.has_field("note") && !alert.validate_note(""))
+            return std::nullopt;
+        if (!alert_json["note"].is_string())
+            return std::nullopt;
+        if (!alert.validate_note(alert_json["note"].as_string()))
+            return std::nullopt;
 
-        if (alert_json.has_field("incidents")) {
-            if (alert_json["incidents"].is_array()) {
-                if (std::optional<std::vector<std::string>> temp_vector = json_array_to_string_vector_optional(
-                        alert_json["incidents"].as_array())) {
-                    if (!alert.validate_incidents(temp_vector.value()))
-                        return std::nullopt;
-                } else {
-                    return std::nullopt;
-                }
-            } else {
-                return std::nullopt;
-            }
-        }
+        std::optional<std::vector<std::string>> references_vector = json_value_to_string_vector_optional(alert_json["references"]);
+        if (!references_vector)
+            return std::nullopt;
+        if (!alert.validate_references(references_vector.value()))
+            return std::nullopt;
+
+        std::optional<std::vector<std::string>> incidents_vector = json_value_to_string_vector_optional(alert_json["incidents"]);
+        if (!incidents_vector)
+            return std::nullopt;
+        if (!alert.validate_incidents(references_vector.value()))
+            return std::nullopt;
 
         if (alert_json.has_field("info")) {
             if (alert_json["info"].is_array()) {
